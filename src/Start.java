34309e0282cafd6fa55cff913574d729efe8536b
CodeArtha <codeartha@gmail.com>
2016-07-27 10:48:43 +0200

    Recreated the start class to aim towards standalone command line app

---
+++ src/Start.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import net.codeartha.playfair.Playfair;
+
+/*
+Syntaxe
+	java Playfair -k
+
+-k/--key			sets the passphrase
+-e/--encrypt		encrypt the message with the provided key
+-d/--decrypt		decrypt the message with the provided key
+-m/--message
+--help
+--version
+
+ */
+
+public class Start {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
